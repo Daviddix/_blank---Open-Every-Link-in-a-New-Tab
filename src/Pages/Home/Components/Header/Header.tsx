bac89bb5b1223c6c81e22a10d92e7ebe00ba7532
Emmanuel Nsikan-David <en56434@gmail.com>
2024-12-22 21:55:55 +0100

    tiny updates

--- src/Pages/Home/Components/Header/Header.tsx
+++ src/Pages/Home/Components/Header/Header.tsx
@@ -3,12 +3,18 @@ import coffeeLogo from "../../../../assets/coffee.svg"
 import "./Header.css"
 
 function Header() {
+  function supportWebsite(){
+    chrome.tabs.create({ url: "https://buymeacoffee.com/david05" })
+  }
+  
   return (
     <header>
           <div className="header-inner">
           <img src={logoIcon} alt="_blank logo" className="logo"/>
 
-          <button className="coffee">
+          <button 
+          onClick={supportWebsite}
+          className="coffee">
             <img src={coffeeLogo} alt="support the developer" />
           </button>
           </div>
